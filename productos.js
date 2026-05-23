1ae7b4c897169797b7419bc26a25a444ec3fd0c6
Valentina <vqlebrunett@gmail.com>
2026-05-23 11:33:35 -0300

    Update productos.js

--- productos.js
+++ productos.js
@@ -1,17 +1,46 @@
 const productos = [
   {
-    id: 1,
+    iid: 1,
     nombre: "Mate de Acero",
     precio: "8500",
     descripcion: "Mate premium con bombilla incluida",
-    imagen: "imagenes/mate.jpg"
+    imagen: "imagenes/mate.jpg",
+    previewUrl: "https://tecito.app/ghostvqle/post/30d41f1d44de4036a03ee428b6fc55f4"
+    
+    
   },
   {
     id: 2,
     nombre: "Remera Oversize",
     precio: "12500",
     descripcion: "Algodón peinado 220gr",
-    imagen: "imagenes/remera.jpg"
+    imagen: "imagenes/remera.jpg",
+    previewUrl: "https://tecito.app/ghostvqle/post/5db5abac98f6474a8ca11a2412fd63d1"
+
+  },
+  {
+    id: 3,
+    nombre: "Tetas",
+    precio: "12533",
+    descripcion: "mis tetas",
+    imagen: "imagenes/tetas.jpg",
+    previewUrl: "https://tecito.app/ghostvqle/post/3f987fc61e57467e89d6a4c2b22cb7a0"
+  },
+  {
+    id: 4,
+    nombre: "coño",
+    precio: "12500",
+    descripcion: "mi coño usado",
+    imagen: "imagenes/coño.jpg",
+    previewUrl: "https://tecito.app/ghostvqle/post/d4f7b01a29674857a01f162692a8fc28"
+  },
+  {
+    id: 5,
+    nombre: "culo",
+    precio: "12500",
+    descripcion: "mi culo feo",
+    imagen: "imagenes/culo.jpg",
+    previewUrl: "https://tecito.app/ghostvqle/post/67b0b35c1abb48598bf286d05efb32b5"
   }
   // Agrega más productos aquí
 ];
